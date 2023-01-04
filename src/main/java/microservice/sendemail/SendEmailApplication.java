cff3049c8dad0a0c7d75a40759897415220c9b19
FueledByRage <erkmgwr@gmail.com>
2023-01-04 13:18:17 -0300

    Working on service implementation

--- src/main/java/microservice/sendemail/SendEmailApplication.java
+++ src/main/java/microservice/sendemail/SendEmailApplication.java
@@ -3,11 +3,11 @@ package microservice.sendemail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SendEmailApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SendEmailApplication.class, args);
 	}
-
 }
--- src/main/java/microservice/sendemail/services/EmailService.java
+++ src/main/java/microservice/sendemail/services/EmailService.java
@@ -1,12 +1,36 @@
 package microservice.sendemail.services;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import microservice.sendemail.repositories.EmailRepository;
+import microservice.sendemail.enums.StatusEmail;
+import microservice.sendemail.models.EmailModel;
+
 
 @Service
 public class EmailService {
     @Autowired
     EmailRepository emailRepository;
+
+    /*@Autowired
+    private JavaMailSender emailSender;*/
+
+    public EmailModel sendEmail( EmailModel emailModel ){
+		emailModel.setSendDate(LocalDateTime.now());
+        try {
+            System.out.println(emailModel);
+            /*SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getFrom());
+            message.setTo(emailModel.getTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);*/
+        } catch (Exception e) {
+            emailModel.setStatus(StatusEmail.ERROR);
+        } 
+        return emailRepository.save(emailModel);
+	}
 }
